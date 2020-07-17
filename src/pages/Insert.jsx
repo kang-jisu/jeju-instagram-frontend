ffcc14649bf1b3ee8df41e08111a7ff80030e208
kang-jisu <rkdwltn0425@gmail.com>
2020-07-17 18:02:42 +0900

    MDB 설치, 이미지 프리뷰 슬라이드 구현

--- src/pages/Insert.jsx
+++ src/pages/Insert.jsx
@@ -1,13 +1,54 @@
 import React, { Component, Fragment } from 'react';
 import {Link, withRouter} from 'react-router-dom';
 
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer } from
+"mdbreact";
+
+
+const Carousel = function(previewList){
+    return (
+        <MDBCarousel
+        activeItem={1}
+        length={previewList.length}
+        showControls={true}
+        showIndicators={true}
+    >
+        <MDBCarouselInner>
+        {
+            previewList.map((pre,index)=>{
+                return(
+                    <MDBCarouselItem itemId={index+1}>
+                    <MDBView>
+                    <img
+                        className="img"
+                        src={pre}
+                        alt="First slide"
+                    />
+                    </MDBView>
+                    </MDBCarouselItem>
+                )
+            })
+        }
+        </MDBCarouselInner>
+    </MDBCarousel>
+    )
+}
 class Insert extends Component {
     constructor(props){
         super(props);
         this.state={
-            imagesList:[],
             description:"",
+            previewList:[],
         }
+    }
+    componentDidMount=()=>{
+        let previewList =  [];
+        for(let i =0; i<this.props.imagesList.length; i++){
+            previewList.push(String(URL.createObjectURL(this.props.imagesList[i])))
+        }
+        this.setState({
+            previewList : previewList,
+        })
     }
     goBack=()=>{
         this.props.history.goBack();
@@ -19,19 +60,8 @@ class Insert extends Component {
         })
     }
 
-    handleFilesChange=(e)=>{
-        /* 해야될것
-         * 개수 10개 이하 설정
-         * 미리보기같은거 . .? 
-         */
-        console.log(e.target.files);
-        this.setState({
-            imagesList:e.target.files,
-        })
-    }
-
     insertForm=(e)=>{
-        alert(String(this.state.imagesList.length)+this.state.description);
+        alert(String(this.props.imagesList.length)+this.state.description);
         
 
         // axios -> [POST] /boards 
@@ -56,12 +86,7 @@ class Insert extends Component {
                 <div className="main-content">
                 <form >
                     <div className="form-wrapper">
-                        <div className="image-div">
-                            {this.props.imagesList.map(image=>{
-                                return (<h1 key={image.name}>{image.name}</h1>)
-                            })}
-                            {String(this.props.imagesList.length)} 
-                        </div>
+                        {Carousel(this.state.previewList)}
                         <div className="text-div">
                             <textarea name="description" value={this.state.description} onChange={this.handleTextChange}/>
                         </div>
